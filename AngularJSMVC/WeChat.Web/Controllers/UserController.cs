bf92437ad288f76e4425e9cc8bb10f2137132deb
keroyang <576710108@qq.com>
2016-07-16 09:45:11 +0800

    route和controller
    添加路由和控制器的操作

---
+++ AngularJSMVC/WeChat.Web/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeChat.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult ListPage()
+        {
+            return View();
+        }
+
+        public ActionResult EditPage(int id)
+        {
+            ViewBag.InfoId = id;
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+       
+    }
+}
--- AngularJSMVC/WeChat.Web/Scripts/AngularMVCApp.js
+++ AngularJSMVC/WeChat.Web/Scripts/AngularMVCApp.js
@@ -1,3 +1,19 @@
-﻿var AngularMVCApp = angular.module('AngularMVCApp', []);
+﻿var AngularMVCApp = angular.module('AngularMVCApp', ['ngRoute']);
 
 AngularMVCApp.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/User/ListPage', {
+            templateUrl: 'User/ListPage'
+        })
+        .when('/User/EditPage/:id', {
+            templateUrl: function(params) { return 'User/EditPage/' + params.id; }
+    }).when('/User/Delete', {
+            templateUrl: '/User/Delete'
+    });
+      
+}
+configFunction.$inject = ['$routeProvider'];
+
+AngularMVCApp.config(configFunction);
